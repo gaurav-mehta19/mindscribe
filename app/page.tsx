eb30e8ab51cea81775bae55d9361642cac00c918
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-06-17 02:02:12 +0530

    Initial Code

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/navbar/navbar";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <Navbar />
+    
+    <Button>
+      Hi
+    </Button>
+    </>
+  );
+}
